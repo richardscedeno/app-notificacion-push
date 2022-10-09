90adea28baeb233031fdff3e21f1ffa506ecc17a
Richards Cedeño <chards.rjcov@gmail.com>
2022-10-09 01:43:37 -0500

    First commit

---
+++ android/app/src/main/java/com/jcedenio/notificaionesPush/MainActivity.java
@@ -0,0 +1,5 @@
+package com.jcedenio.notificaionesPush;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+//mis importaciones
+import { Platform } from '@ionic/angular';
+
+import {
+  ActionPerformed,
+  PushNotificationSchema,
+  PushNotifications,
+  Token,
+} from '@capacitor/push-notifications';
+
+//const { notifiionPush } = Plugin;
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
-  constructor() {}
+export class HomePage implements OnInit{
 
+  constructor(private platform:Platform) {}
+
+  ngOnInit() {
+    console.log('Inicializando...');
+
+    if(this.platform.is('capacitor')){
+
+      // Request permission to use push notifications
+      // iOS will prompt user and return if they granted permission or not
+      // Android will just grant without prompting
+      PushNotifications.requestPermissions().then(result => {
+        if (result.receive === 'granted') {
+          console.log('Permisos concedidos');
+          // Register with Apple / Google to receive push via APNS/FCM
+          PushNotifications.register();
+        } else {
+          // Show some error
+        }
+      });
+    
+      PushNotifications.addListener('registration', (token: Token) => {
+        alert('Push registration success, token: ' + token.value);
+      });
+      
+      PushNotifications.addListener('registrationError', (error: any) => {
+        alert('Error on registration: ' + JSON.stringify(error));
+      });
+      
+
+      //Se ejecuta cuando está en primer plano la app (cuando está abierta)
+      PushNotifications.addListener(
+        'pushNotificationReceived',
+        (notification: PushNotificationSchema) => {
+          alert('Push received: ' + JSON.stringify(notification));
+        },
+      );
+      
+      PushNotifications.addListener(
+        'pushNotificationActionPerformed',
+        (notification: ActionPerformed) => {
+          alert('Push action performed: ' + JSON.stringify(notification));
+        },
+      );
+
+    }else{
+      console.log('PushNotifications.requestPermission() -> No es un dispositivo movil');
+    }
+  }
 }
